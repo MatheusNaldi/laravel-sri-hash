babbecd0923b4d27694c3ac293d8293202eb9f87
Matheus Naldi <matheus_naldi@hotmail.com>
2019-11-14 11:24:36 -0300

    Added compatibility with laravel 6

--- src/Commands/GenerateSriHashes.php
+++ src/Commands/GenerateSriHashes.php
@@ -36,7 +36,7 @@ class GenerateSriHashes extends Command
     /**
      * Execute the console command.
      *
-     * @return mixed
+     * @return void
      */
     public function handle()
     {
